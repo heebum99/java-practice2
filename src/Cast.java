47e4b8bec489f001aa71b85e898673877ef7cbac
heebum99 <heebum9955@naver.com>
2022-08-07 21:24:41 +0900

    캐스트 연산자 -> (), 형 변환

---
+++ src/Cast.java
@@ -0,0 +1,7 @@
+public class Cast {
+    public static void main(String[] args) {
+        //float 형으로 형 변환(캐스트)
+        System.out.println("3/2 = "+(float)3/2);
+        System.out.println("3/2 = "+3/(float)2);
+    }
+}

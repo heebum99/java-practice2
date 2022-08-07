30eb01b9a587eb639c1822de6dd97e2a9e5a2db1
heebum99 <heebum9955@naver.com>
2022-08-07 19:15:45 +0900

    도전 문제 1-4

---
+++ src/Test104.java
@@ -0,0 +1,11 @@
+public class Test104 {
+    public static void main(String[] args) {
+
+        String [] name = {"John","Kim"};
+
+        System.out.println(name[0]+" "+name[1]);
+        System.out.println(name[0]);
+        System.out.println(name[1]);
+        System.out.println(name.length);
+    }
+}

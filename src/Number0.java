224bb587f47de4b16fc131d769898fcce1732bf9
heebum99 <heebum9955@naver.com>
2022-08-07 18:50:08 +0900

    배열

---
+++ src/Number0.java
@@ -0,0 +1,11 @@
+public class Number0 {
+    public static void main(String[] args) {
+
+        //배열 초기화
+        int [] a = new int [] {1,2,3,4};
+        int b [] = {4,5,6,7};
+
+        System.out.println(a[0]);
+        System.out.println(b[0]);
+    }
+}

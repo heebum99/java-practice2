a0f4c0ca43e0c98e80f32c9ca9c6592d1c49cf9d
heebum99 <heebum9955@naver.com>
2022-08-07 19:30:07 +0900

    대입 연산자(=,+=,-=...)

---
+++ src/Plus.java
@@ -0,0 +1,7 @@
+public class Plus {
+    public static void main(String[] args) {
+        int a = 90;
+        a += 10; //a = a + 10;
+        System.out.println("90에 10을 더하면 "+a+"입니다.");
+    }
+}

b81ef48d3f78e00397e365f82e992cfb1a7e4db4
heebum99 <heebum9955@naver.com>
2022-08-07 20:54:37 +0900

    산술 연산자, 전치 ++a, 후치 a++

---
+++ src/Position.java
@@ -0,0 +1,7 @@
+public class Position {
+    public static void main(String[] args) {
+        int a = 1, b = 1;
+        System.out.println("전치일 경우 "+ ++a + "가 됩니다.");
+        System.out.println("후치일 경우 "+ b++ + "이 됩니다.");
+    }
+}

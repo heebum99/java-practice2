b676a3fa12e733d2fd55b087c6dd7c8ad0a26c87
heebum99 <heebum9955@naver.com>
2022-08-07 21:29:11 +0900

    연산 우선순위

---
+++ src/Priority.java
@@ -0,0 +1,8 @@
+public class Priority {
+    public static void main(String[] args) {
+        System.out.println("2x8-6/2 = "+(2*8-6/2));
+        System.out.println("2x(8-6)/2 = "+(2*(8-6)/2));
+        System.out.println("1-2+3 = "+(1-2+3));
+        System.out.println("1-(2+3) = "+(1-(2+3)));
+    }
+}

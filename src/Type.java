9f762691d21f164cf7b240e4b021d29e9157db2c
heebum99 <heebum9955@naver.com>
2022-08-07 21:21:27 +0900

    계산 중 형변환(정수/정수, 실수/정수, 실수/실수)

---
+++ src/Type.java
@@ -0,0 +1,11 @@
+public class Type {
+    public static void main(String[] args) {
+        // 정수/정수 -> 정수
+        System.out.println("3/2 = "+(3/2)); 
+        
+        //실수를 포함해 계산 -> 실수
+        System.out.println("3.0/2.0 = "+(3.0/2.0));
+        System.out.println("3.0/2 = "+(3.0/2));
+        System.out.println("3/2.0 = "+(3/2.0));
+    }
+}

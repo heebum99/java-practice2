fd8005c931a3b8e7b3c4b11a047e16850cf2f6d7
heebum99 <heebum9955@naver.com>
2022-08-07 18:38:51 +0900

    수치형(정수형, 실수형)

---
+++ src/Data2.java
@@ -0,0 +1,15 @@
+public class Data2 {
+    public static void main(String[] args) {
+        
+        //변수 초기화
+        byte a = 127;
+        short b = 10000;
+        float c = 1.2f;
+        double d = 0.34;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}

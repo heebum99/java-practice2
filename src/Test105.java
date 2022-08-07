d0c3b974e477182d396f00277483258a5351faa5
heebum99 <heebum9955@naver.com>
2022-08-07 19:17:29 +0900

    도전 문제 1-5

---
+++ src/Test105.java
@@ -0,0 +1,7 @@
+public class Test105 {
+    public static void main(String[] args) {
+        String [] Info = {"김희범","안양시 동안구"};
+        System.out.println(Info[0]);
+        System.out.println(Info[1]);
+    }
+}

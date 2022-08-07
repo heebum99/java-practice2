facb22ec8413bd286e3f3513a6d1650101c023bb
heebum99 <heebum9955@naver.com>
2022-08-07 19:26:22 +0900

    수치 계산에 사용되는 연산자

---
+++ src/Calculation.java
@@ -0,0 +1,9 @@
+public class Calculation {
+    public static void main(String[] args) {
+        System.out.println("5+5는 "+(5+5)+"입니다.");
+        System.out.println("5-5는 "+(5-5)+"입니다.");
+        System.out.println("5x5는 "+(5*5)+"입니다.");
+        System.out.println("5/5는 "+(5/5)+"입니다.");
+        System.out.println("5%3의 나머지는 "+(5%3)+"입니다.");
+    }
+}

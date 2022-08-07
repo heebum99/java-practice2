4903c055f803106bd23e09344952666c47b6ee78
heebum99 <heebum9955@naver.com>
2022-08-07 21:17:53 +0900

    n진수(2진수, 8진수, 10진수, 16진수)표현법

---
+++ src/BinaryCode.java
@@ -0,0 +1,8 @@
+public class BinaryCode {
+    public static void main(String[] args) {
+        System.out.println(0x10); //16진수 10을 표현 0x -> 16
+        System.out.println(10); //10진수 10을 표현 -> 10
+        System.out.println(010); //8진수 10을 표현 0 -> 8
+        System.out.println(0b10); //2진수 10을 표현 0b -> 2
+    }
+}

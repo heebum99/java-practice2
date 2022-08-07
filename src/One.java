75e46304c8dd77d6e79708e5dd264e8e4c09091c
heebum99 <heebum9955@naver.com>
2022-08-07 20:50:59 +0900

    산술 연산자(증감 연산자 ++,--)

---
+++ src/One.java
@@ -0,0 +1,12 @@
+public class One {
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.println("처음 값은 "+a+"이었습니다.");
+
+        a++;
+        System.out.println("1 증가해서 "+a+"가 됩니다.");
+
+        a--;
+        System.out.println("1 감소해서 "+a+"로 돌아왔습니다.");
+    }
+}

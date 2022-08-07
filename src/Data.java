23a8b9880095a51c3ed6918aea46ee448e2de925
heebum99 <heebum9955@naver.com>
2022-08-07 18:33:15 +0900

    변수

---
+++ src/Data.java
@@ -0,0 +1,18 @@
+public class Data {
+    public static void main(String[] args) {
+
+        //변수 선언
+        int a;
+        int b;
+
+        //변수에 값 대입
+        a = 2;
+        b = 3;
+        a = b;
+
+        //변수 초기화 => 선언과 대입을 동시에
+        int c = 5;
+
+        System.out.println(a);
+    }
+}

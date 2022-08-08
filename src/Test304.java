e65d4b96d6eb7b3618d4faafffd69ec5eff3c5fd
heebum99 <heebum9955@naver.com>
2022-08-08 17:46:33 +0900

    도전3-4 알고리즘 - 중첩 반복문으로 * 출력

---
+++ src/Test304.java
@@ -0,0 +1,13 @@
+public class Test304 {
+    public static void main(String[] args) {
+        for(int i=0;i<4;i++){
+            for(int j=2;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int k=0;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

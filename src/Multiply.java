7e2b29c566ebfda9268e61a68e16e314eccaf8b9
heebum99 <heebum9955@naver.com>
2022-08-08 16:30:23 +0900

    다중 for 반복문 - 구구단

---
+++ src/Multiply.java
@@ -0,0 +1,11 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int i,j;
+
+        for(j=1;j<=9;j++){
+            for(i=1;i<=9;i++){
+                System.out.println(j+"x"+i+"="+j*i);
+            }
+        }
+    }
+}

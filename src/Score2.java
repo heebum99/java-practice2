143332262b3c8e956170a5954f2c151c73618c97
heebum99 <heebum9955@naver.com>
2022-08-08 16:26:41 +0900

    중첩 if문

---
+++ src/Score2.java
@@ -0,0 +1,17 @@
+public class Score2 {
+    public static void main(String[] args) {
+        int a = 90;
+
+        if(a > 80){
+            if(a == 100){
+                System.out.println("만점입니다.");
+            }
+            else {
+                System.out.println("조금 더 노력하세요.");
+            }
+        }
+        else {
+            System.out.println("힘내세요.");
+        }
+    }
+}

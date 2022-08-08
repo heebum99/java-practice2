835fd42f03092547184a5a0bd3df02f838f0f3c3
heebum99 <heebum9955@naver.com>
2022-08-08 16:20:00 +0900

    if-else 조건문, 블록으로 복수처리

---
+++ src/Score.java
@@ -0,0 +1,15 @@
+public class Score {
+    public static void main(String[] args) {
+        int s = 65;
+
+        System.out.println("당신의 점수는 "+s+"점 입니다.");
+
+        if(s < 70){
+            System.out.println("평균까지 앞으로 "+(70-s)+"점");
+            System.out.println("힘내세요!");
+        }
+        else{
+            System.out.println("잘 하셨습니다.");
+        }
+    }
+}

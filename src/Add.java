4db0785a4d8d71d57e290b91a1299d7bec461539
heebum99 <heebum9955@naver.com>
2022-08-12 15:39:32 +0900

    문자열을 분할 - StringTokenizer

---
+++ src/Add.java
@@ -0,0 +1,17 @@
+import java.util.StringTokenizer;
+
+public class Add {
+    public static void main(String[] args) {
+        StringTokenizer v = new StringTokenizer("a b c");
+        StringTokenizer n = new StringTokenizer("1 2 3");
+
+        String s = v.nextToken();
+        int sum = Integer.parseInt(n.nextToken());
+
+        while(v.hasMoreTokens()){
+            s = s + " + " + (v.nextToken());
+            sum = sum + Integer.parseInt(n.nextToken());
+        }
+        System.out.println(s + " = "+sum);
+    }
+}

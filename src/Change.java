d856295556e1ee2972ade38433ae48eb0ae9eb37
heebum99 <heebum9955@naver.com>
2022-08-12 15:28:21 +0900

    랩퍼 클래스

---
+++ src/Change.java
@@ -0,0 +1,9 @@
+public class Change {
+    public static void main(String[] args) {
+        String a = "34";
+        int b = 50;
+        int c = Integer.parseInt(a); //문자열 34를 정수형 34로 변환
+        int d = c + b;
+        System.out.println(a+" + "+b+" = "+ d);
+    }
+}
